39bfef50ebe4ed3e0f48052777a40c2c8fc215f3
Tim <timothy.m.martin@gmail.com>
2015-10-23 11:49:20 -0400

    Syntax changes.

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,12 @@ module.exports = {
         publicPath: 'http://localhost:8080/assets'
     },
     module: {
-        loaders: [
-            {test: /\.jsx$/,  loader: 'jsx-loader?insertPragma=React.DOM&harmony'},
-            {test: /\.css$/,   loader: 'style-loader!css-loader'}
-        ]
+        loaders: [{
+            test: /\.jsx$/,
+            loader: 'jsx-loader?insertPragma=React.DOM&harmony'
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }]
     },
-}
+};
